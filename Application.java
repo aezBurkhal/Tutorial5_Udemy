56a203d3ba49558b2e168498eb9aff9fe6bda8a6
aezBurkhal <44988851+aezBurkhal@users.noreply.github.com>
2018-12-12 00:27:08 -0500

    Update Application.java

--- Application.java
+++ Application.java
@@ -3,7 +3,7 @@ public class Application {
 	public static void main(String[] args) {
 
 		for(int i=0; i < 5; i++) {
-			//printf does not make output go on new line
+			//printf keeps output on same line
 			// \n means print new line
 			
 			System.out.printf("The value of i is: %d\n", i);
