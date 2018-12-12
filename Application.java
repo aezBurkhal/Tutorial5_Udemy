c517d9237d5a271f2d5066b25c5e59cb965fe34b
aezBurkhal <44988851+aezBurkhal@users.noreply.github.com>
2018-12-12 00:23:28 -0500

    Add files via upload

---
+++ Application.java
@@ -0,0 +1,14 @@
+
+public class Application {
+	public static void main(String[] args) {
+
+		for(int i=0; i < 5; i++) {
+			//printf does not make output go on new line
+			// \n means print new line
+			
+			System.out.printf("The value of i is: %d\n", i);
+			
+			
+		}
+	}
+}
